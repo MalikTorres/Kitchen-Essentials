a0ebbaab09b98109c4d36a9275e0b12e3dedb206
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-01-25 23:52:27 -0500

    Updated coniditional statements to correctly present options for cooking

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ let semiFrequent = 6;
 
 let leastFrequent = 4; 
 
-if(cookingFrequency ===  veryFrequent > 6 && veryFrequent <= 10) {
+if(cookingFrequency <= veryFrequent && cookingFrequency > semiFrequent) {
 
     document.write("A sous vide would be a valuable project to take your cooking to the next level");
 
-} else if (cookingFrequency === semiFrequent > 4 && semiFrequent <= 6){ 
+} else if (cookingFrequency <= semiFrequent && cookingFrequency > leastFrequent){ 
 
     document.write("An instapot would be the perfect addtion to your kitchen!");
 
-} else if (cookingFrequency === leastFrequent > 0 && leastFrequent <= 4) {
+} else if (cookingFrequency <= leastFrequent) {
 
-    document.write("Perhaps start off with chefs knife")
+    document.write("Perhaps start off with a chefs knife")
 
-} else if (cookingFrequency !==  veryFrequent  || cookingFrequency !== semiFrequent || leastFrequent ) {
+} else {
 
     document.write("Please click the link above which has great restaurant suggestions")
 } 
