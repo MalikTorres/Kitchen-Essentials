9abcc00dab1c662f1fbdf8ccbb48a8d1c45aaebb
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-01-25 19:11:57 -0500

    Added user question to determine how often they cook and then prompt an answer

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let cookingFrequency = prompt("How how many times per week do you cook?");
+
+
+
+let veryFrequent = 10;
+
+let semiFrequent = 6; 
+
+let leastFrequent = 4; 
+
+if(cookingFrequency ===  veryFrequent || veryFrequent <= 10) {
+
+    document.write("A sous vide would be a valuable project to take your cooking to the next level");
+
+} else if {
+
+}
+
