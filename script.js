0b01a1fa11586b6e3d3808e42cee54f3944e1c84
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-01-25 23:20:32 -0500

    Added greeting and questions to confirm how many times per week the user cooks in their kitchen

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
 //FIRST DYNAMIC COMPONENT
+
+
+// SECOND DYNAMIC COMPONENT
+
+
+let nameConfirm = prompt("What is your name?") 
+
+let greeting = prompt("Hi" + " " + nameConfirm + "!" + "please click OK below");
+
+// THIRD DYNAMIC COMPONENT
 let cookingFrequency = prompt("How how many times per week do you cook?");
 
 let veryFrequent = 10;
@@ -8,22 +18,26 @@ let semiFrequent = 6;
 
 let leastFrequent = 4; 
 
-if(cookingFrequency ===  veryFrequent || veryFrequent <= 10) {
+if(cookingFrequency ===  veryFrequent > 6 && veryFrequent <= 10) {
 
     document.write("A sous vide would be a valuable project to take your cooking to the next level");
 
-} else if (cookingFrequency === semiFrequent || semiFrequent <= 6){ 
+} else if (cookingFrequency === semiFrequent > 4 && semiFrequent <= 6){ 
 
     document.write("An instapot would be the perfect addtion to your kitchen!");
 
-} else if (cookingFrequency === leastFrequent || leastFrequent <= 4) {
+} else if (cookingFrequency === leastFrequent > 0 && leastFrequent <= 4) {
 
     document.write("Perhaps start off with chefs knife")
 
 } else if (cookingFrequency !==  veryFrequent  || cookingFrequency !== semiFrequent || leastFrequent ) {
 
     document.write("Please click the link above which has great restaurant suggestions")
-}
+} 
+
+
+
+let confirimation = comfirm('Are you having a good day today?')
 
 
 
