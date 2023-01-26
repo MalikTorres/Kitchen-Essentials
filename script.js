24a6f18348ec94aecb5c9b6d7837df1767ff3b79
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-01-25 19:29:02 -0500

    Completed adding conditional statements based on user answers

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
-
+//FIRST DYNAMIC COMPONENT
 let cookingFrequency = prompt("How how many times per week do you cook?");
-
-
 
 let veryFrequent = 10;
 
@@ -14,7 +12,18 @@ if(cookingFrequency ===  veryFrequent || veryFrequent <= 10) {
 
     document.write("A sous vide would be a valuable project to take your cooking to the next level");
 
-} else if {
+} else if (cookingFrequency === semiFrequent || semiFrequent <= 6){ 
 
+    document.write("An instapot would be the perfect addtion to your kitchen!");
+
+} else if (cookingFrequency === leastFrequent || leastFrequent <= 4) {
+
+    document.write("Perhaps start off with chefs knife")
+
+} else if (cookingFrequency !==  veryFrequent  || cookingFrequency !== semiFrequent || leastFrequent ) {
+
+    document.write("Please click the link above which has great restaurant suggestions")
 }
+
+
 
