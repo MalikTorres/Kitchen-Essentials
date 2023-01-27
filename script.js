0e7cc11e171ed93f1063a7447e80b45767ea1705
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-01-26 19:16:43 -0500

    Added conditional statement to confirm the users name, if they select cancel or do not put their name int he prompt they will not be able to proceed

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-
+function howOftenDoYouCook() {
 //FIRST DYNAMIC COMPONENT
 let nameConfirm = prompt("What is your name?") 
 
 // SECOND DYNAMIC COMPONENT
-let greeting = alert("Hi" + " " + nameConfirm + "!" + " " + "Please click OK to continue");
+if(!nameConfirm) {
+    
+    howOftenDoYouCook()
+
+} else { 
+
+    alert("Hi" + " " + nameConfirm + "!" + " " + "Please click OK to continue");
+} 
 
 // THIRD DYNAMIC COMPONENT
 let cookingFrequency = prompt("How how many times per week do you cook?");
@@ -14,24 +21,23 @@ let semiFrequent = 6;
 
 let leastFrequent = 4; 
 
-if(cookingFrequency <= veryFrequent && cookingFrequency > semiFrequent) {
+     if (cookingFrequency <= veryFrequent && cookingFrequency > semiFrequent) { 
+    
+        document.write("A sous vide would be a valuable project to take your cooking to the next level");
 
-    document.write("A sous vide would be a valuable project to take your cooking to the next level");
-
-} else if (cookingFrequency <= semiFrequent && cookingFrequency > leastFrequent){ 
+    } else if (cookingFrequency <= semiFrequent && cookingFrequency > leastFrequent) { 
 
     document.write("An instapot would be the perfect addtion to your kitchen!");
 
-} else if (cookingFrequency <= leastFrequent) {
+   } else if (cookingFrequency <= leastFrequent) {
 
-    document.write("Perhaps start off with a chefs knife")
+    document.write("Perhaps start off with a chefs knife");
 
-} else {
+    } else {
+    
+    document.write("Please click the link above which has great restaurant suggestions");
+ } 
 
-    document.write("Please click the link above which has great restaurant suggestions")
 } 
 
-
-
-
-
+howOftenDoYouCook();
