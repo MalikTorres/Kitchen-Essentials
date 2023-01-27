52bba54756ed94bbd5a528008c0bbd7d7972119a
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-01-27 02:34:58 -0500

    updated link for image

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function pictureOfSousVide() {
     
     for(let i=0; i < likelyHoodToBuyProduct; i++) {
 
-        answer += "<img class= 'sous-vide' src= 'SousVideMachine.jpg'/>"
+        answer += "<img class= 'sous-vide' src= 'Sousvidephoto.jpg'/>"
     }
 
       return document.write(answer);
